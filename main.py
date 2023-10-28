f4b1b1d20d1b9b069d7d38b2b0d2148d59af38a7
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-10-28 12:51:31 -0300

    add basic implementaion of classes OrderBook and  TestOrderBook

--- main.py
+++ main.py
@@ -23,7 +23,28 @@ class Order:
         self.price = price
 
     def __repr__(self):
-        return f"Order(id={self.order_id}, type={self.type}, qty={self.quantity}, price={self.price}, time={self.timestamp})"
+        return (
+            f"Order(id={self.order_id}, type={self.type}, qty={self.quantity}, "
+            f"price={self.price}, time={self.timestamp})"
+        )
+
+
+class OrderBook:
+    def __init__(self):
+        self.buy_orders = []
+        self.sell_orders = []
+
+    def add_order(self, order):
+        if order.type == OrderType.BUY:
+            self.buy_orders.append(order)
+        elif order.type == OrderType.SELL:
+            self.sell_orders.append(order)
+
+    def remove_order(self, order):
+        if order in self.buy_orders:
+            self.buy_orders.remove(order)
+        elif order in self.sell_orders:
+            self.sell_orders.remove(order)
 
 
 def generate_random_order():
@@ -44,12 +65,39 @@ class TestOrder(unittest.TestCase):
 
     def test_order_representation(self):
         order = Order(1, OrderType.SELL, 5, 200)
-        expected_repr = f"Order(id={order.order_id}, type={order.type}, qty={order.quantity}, price={order.price}, time={order.timestamp})"
+        expected_repr = (
+            f"Order(id={order.order_id}, type={order.type}, qty={order.quantity}, "
+            f"price={order.price}, time={order.timestamp})"
+        )
         self.assertEqual(repr(order), expected_repr)
 
     def test_invalid_order_type(self):
         with self.assertRaises(ValueError):
             Order(1, "invalid", 3, 150)
+
+
+class TestOrderBook(unittest.TestCase):
+    def setUp(self):
+        self.order_book = OrderBook()
+
+    def test_add_buy_order(self):
+        buy_order = Order(1, OrderType.BUY, 10, 100)
+        self.order_book.add_order(buy_order)
+        self.assertEqual(len(self.order_book.buy_orders), 1)
+        self.assertEqual(len(self.order_book.sell_orders), 0)
+
+    def test_add_sell_order(self):
+        sell_order = Order(2, OrderType.SELL, 5, 200)
+        self.order_book.add_order(sell_order)
+        self.assertEqual(len(self.order_book.buy_orders), 0)
+        self.assertEqual(len(self.order_book.sell_orders), 1)
+
+    def test_remove_order(self):
+        buy_order = Order(1, OrderType.BUY, 10, 100)
+        self.order_book.add_order(buy_order)
+        self.order_book.remove_order(buy_order)
+        self.assertEqual(len(self.order_book.buy_orders), 0)
+        self.assertEqual(len(self.order_book.sell_orders), 0)
 
 
 def main():
